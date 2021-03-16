f64b1bdfed24715c97928ae5c236821cb2d8f214
Sienna94 <csj19940407@gmail.com>
2021-03-16 21:52:02 +0900

    facebook signin

--- app/src/main/java/com/example/ggugram/LoginActivity.kt
+++ app/src/main/java/com/example/ggugram/LoginActivity.kt
@@ -1,24 +1,39 @@
 package com.example.ggugram
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Base64
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.util.*
+
 
 class LoginActivity : AppCompatActivity() {
     var auth: FirebaseAuth? = null
     var googleSignInClient: GoogleSignInClient? = null
     var GOOGLE_LOGIN_CODE = 9001
+    var callbackManager : CallbackManager? = null //facebook 로그인 결과 가져오는 콜백
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -26,6 +41,7 @@ class LoginActivity : AppCompatActivity() {
         btn_login.setOnClickListener {
             signin_signup()
         }
+        //GOOGLE
         var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
@@ -34,9 +50,69 @@ class LoginActivity : AppCompatActivity() {
         btn_google_login.setOnClickListener {
             //1st step
             googleLogin()
+            //facebook hash값 확인위해 임시로 넣어줌
+            //printHashKey()
+        }
+        //FACEBOOK
+        btn_facebook_login.setOnClickListener {
+            //1st step
+            facebookLogin()
+        }
+        //facebook 콜백
+        callbackManager = CallbackManager.Factory.create()
+    }
+    // Hash Key: Ok6oj/VRWZLaHE2wr/QHdQiBv/k=
+
+    //facebook login
+    open fun printHashKey() {
+        try {
+            val info: PackageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+            for (signature in info.signatures) {
+                val md: MessageDigest = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val hashKey: String = String(Base64.encode(md.digest(), 0))
+                Log.i("TAG", "printHashKey() Hash Key: $hashKey")
+            }
+        } catch (e: NoSuchAlgorithmException) {
+            Log.e("TAG", "printHashKey()", e)
+        } catch (e: Exception) {
+            Log.e("TAG", "printHashKey()", e)
         }
     }
+    //페북 로그인 function
+    fun facebookLogin(){
+        LoginManager.getInstance()
+            .logInWithReadPermissions(this, Arrays.asList("public_profile", "email")) //facebook에서 받을 권한 요청
+        LoginManager.getInstance()
+            .registerCallback(callbackManager, object : FacebookCallback<LoginResult>{ //object를 implement해서 function들을 추가해준 것.
+                override fun onSuccess(result: LoginResult?) { //로그인이 성공하면 facebook 데이터를 firebase에 넘김
+                    //2nd step
+                    handleFacebookAccessToken(result?.accessToken) //아래에 function 만들어 둠.
+                }
+                override fun onCancel() {
+                }
+                override fun onError(error: FacebookException?) {
+                }
+            })
+    }
+    fun handleFacebookAccessToken(token : AccessToken?) { //토큰 받아주는 파라미터
+        var credential = FacebookAuthProvider.getCredential(token?.token!!)
+        auth?.signInWithCredential(credential) //바로 firebase로 넘겨줌 (이후 구글 로그인과 동일)
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) { //3rd step 응답값을 받아서 페이지를 이동시킴
+                    //Login ok: 아이디와 패스워드가 일치할 때 (로그인 성공시) 메인으로 넘어가는 코드
+                    Log.d("ggl", "firebaseAuthWithGoogle: succeed")
+                    moveMainPage(task.result?.user)
+                } else {
+                    //Login fail : 아이디 패스워드 불일치 ->show the error msg
+                    Log.d("ggl", "firebaseAuthWithGoogle: failed")
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG)
+                        .show() //에러나면 toast 띄우기
+                }
+            }
+    }
 
+    //구글 로그인 function
     fun googleLogin() {
         var signInIntent = googleSignInClient?.signInIntent
         startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
@@ -44,6 +120,7 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+        callbackManager?.onActivityResult(requestCode, resultCode, data)
         if (requestCode == GOOGLE_LOGIN_CODE) {
             var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
             if (result != null) {
@@ -63,8 +140,7 @@ class LoginActivity : AppCompatActivity() {
     fun firebaseAuthWithGoogle(account: GoogleSignInAccount) {
         var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
         auth?.signInWithCredential(credential)
-            ?.addOnCompleteListener {
-                    task ->
+            ?.addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     //Login ok: 아이디와 패스워드가 일치할 때 (로그인 성공시) 메인으로 넘어가는 코드
                     Log.d("ggl", "firebaseAuthWithGoogle: succeed")
